28186d68a1a0f5b387eff2a8cf7c21ca31e30c1c
lanjian <272976672@qq.com>
2019-05-04 20:12:50 +0800

    init

---
+++ ego-parent/src/main/java/com/lanjian/App.java
@@ -0,0 +1,19 @@
+package com.lanjian;
+
+
+public class App {
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
